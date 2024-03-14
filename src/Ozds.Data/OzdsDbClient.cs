71224f92b3f0773552dbb2514e7cb6c7deea7d09
Hrvoje Jurić <hrvoje@altibiz.com>
2024-03-14 13:21:16 +0100

    WIP

--- src/Ozds.Data/OzdsDbClient.cs
+++ src/Ozds.Data/OzdsDbClient.cs
@@ -1,14 +1,11 @@
-using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
-using Ozds.Data.Entities;
 
 namespace Ozds.Data;
 
 public partial class OzdsDbClient(OzdsDbContext context)
 {
   public async Task MigrateAsync()
-{
-  await context.Database.MigrateAsync();
-}
+  {
+    await context.Database.MigrateAsync();
+  }
 }
