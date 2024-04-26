bd4ea6f21b95c15f0ec4f49cbbbad025948aae6e
Hrvoje Juric <hrvoje@altibiz.com>
2024-04-26 15:43:40 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -33,59 +33,65 @@
           PGUSER = "ozds";
           PGPASSWORD = "ozds";
 
-          packages = with pkgs; let
-            usql = writeShellApplication {
-              name = "usql";
-              runtimeInputs = [ usql ];
-              text = ''
-                usql pg://ozds:ozds@localhost/ozds?sslmode=disable "$@"
-              '';
-            };
-            mermerd = writeShellApplication {
-              name = "mermerd";
-              runtimeInputs = [ mermerd ];
-              text = ''
-                mermerd --connectionString postgresql://ozds:ozds@localhost:5432/ozds "$@"
-              '';
-            };
-          in
-          [
-            # Version Control
-            git
-            dvc-with-remotes
+          packages =
+            let
+              usql = pkgs.writeShellApplication {
+                name = "usql";
+                runtimeInputs = [ pkgs.usql ];
+                text = ''
+                  usql \
+                    pg://ozds:ozds@localhost/ozds?sslmode=disable \
+                    "$@"
+                '';
+              };
+              mermerd = pkgs.writeShellApplication {
+                name = "mermerd";
+                runtimeInputs = [ pkgs.mermerd ];
+                text = ''
+                  mermerd \
+                    --connectionString postgresql://ozds:ozds@localhost:5432/ozds \
+                    "$@"
+                '';
+              };
+            in
+            with pkgs;
+            [
+              # Version Control
+              git
+              dvc-with-remotes
 
-            # Nix
-            nil
-            nixpkgs-fmt
+              # Nix
+              nil
+              nixpkgs-fmt
 
-            # C#
-            dotnet-sdk
-            dotnet-runtime
-            dotnet-aspnetcore
-            omnisharp-roslyn
-            netcoredbg
+              # C#
+              dotnet-sdk
+              dotnet-runtime
+              dotnet-aspnetcore
+              omnisharp-roslyn
+              netcoredbg
 
-            # PostgreSQL
-            usql
-            postgresql_14
-            mermerd
+              # PostgreSQL
+              usql
+              postgresql_14
+              mermerd
 
-            # Scripts
-            just
-            nushell
+              # Scripts
+              just
+              nushell
 
-            # Documentation
-            doxygen
-            simple-http-server
+              # Documentation
+              doxygen
+              simple-http-server
 
-            # Misc
-            nodePackages.prettier
-            nodePackages.yaml-language-server
-            nodePackages.vscode-langservers-extracted
-            taplo
-            marksman
-            nodePackages.cspell
-          ];
+              # Misc
+              nodePackages.prettier
+              nodePackages.yaml-language-server
+              nodePackages.vscode-langservers-extracted
+              taplo
+              marksman
+              nodePackages.cspell
+            ];
         };
       });
 }
