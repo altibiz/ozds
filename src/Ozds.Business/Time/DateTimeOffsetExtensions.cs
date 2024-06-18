46151ea2e11d349c6a2dfbace21556b7dfa9063f
Gabrijel Čmelić <gabrijel@altibiz.com>
2024-06-18 14:28:07 +0200

    WIP

--- src/Ozds.Business/Time/DateTimeOffsetExtensions.cs
+++ src/Ozds.Business/Time/DateTimeOffsetExtensions.cs
@@ -1,14 +1,43 @@
+using System.Globalization;
+
 namespace Ozds.Business.Time;
 
 public static class DateTimeOffsetExtensions
 {
   // NOTE: Croatian UTC offset (https://en.wikipedia.org/wiki/List_of_UTC_offsets)
-  public static readonly TimeSpan DefaultOffset = TimeSpan.FromHours(1);
+
+  public static TimeSpan DefaultOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+  private static readonly Dictionary<string, string> CultureTimeZoneMap = new Dictionary<string, string>
+    {
+        { "en-US", "Eastern Standard Time" },
+        { "hr-HR", "Central European Standard Time" },
+    };
+  public static void GetTimeOffsetForCurrentCulture()
+  {
+    var currentCulture = CultureInfo.CurrentCulture.Name;
+
+    if (CultureTimeZoneMap.TryGetValue(currentCulture, out string? timeZoneId))
+    {
+      var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+
+      var offset = timeZone.GetUtcOffset(DateTime.UtcNow);
+
+      DefaultOffset = offset;
+    }
+    else
+    {
+      var localTimeZone = TimeZoneInfo.Local;
+      DefaultOffset = localTimeZone.GetUtcOffset(DateTime.UtcNow);
+    }
+  }
+  public static readonly TimeSpan OldDefaultOffset = TimeSpan.FromHours(1);
+
 
   public static (DateTimeOffset, DateTimeOffset) GetMonthRange(
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     var monthStart = new DateTimeOffset(
       dateTimeOffset.Year,
@@ -27,6 +56,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     var quarterHour = dateTimeOffset.Minute / 15;
     return new DateTimeOffset(
@@ -44,6 +74,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return new DateTimeOffset(
       dateTimeOffset.Year,
@@ -60,6 +91,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return new DateTimeOffset(
       dateTimeOffset.Year,
@@ -76,6 +108,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return new DateTimeOffset(
       dateTimeOffset.Year,
@@ -92,6 +125,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return new DateTimeOffset(
       dateTimeOffset.Year,
@@ -108,6 +142,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return new DateTimeOffset(
       dateTimeOffset.Year,
@@ -124,6 +159,7 @@ public static class DateTimeOffsetExtensions
     this DateTimeOffset dateTimeOffset
   )
   {
+    GetTimeOffsetForCurrentCulture();
     dateTimeOffset = dateTimeOffset.ToOffset(DefaultOffset);
     return Enumerable
       .Range(1, 12).Select(
