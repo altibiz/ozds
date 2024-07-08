b10f7ce314ce02ca3229a88a4a50a15891459c90
GabrijelAB <GabrijelAB@users.noreply.github.com>
2024-07-08 13:51:06 +0000

    format

--- src/Ozds.Business/Math/PhasicMeasure.cs
+++ src/Ozds.Business/Math/PhasicMeasure.cs
@@ -234,12 +234,12 @@ public abstract record class PhasicMeasure<T>
     return this switch
     {
       CompositePhasicMeasure<T> composite => composite.Measures.OrderBy(
-            measure => measure switch
-            {
-              SinglePhasicMeasureSum<T> => 0,
-              TriPhasicMeasure<T> => 1,
-              _ => 2
-            })
+          measure => measure switch
+          {
+            SinglePhasicMeasureSum<T> => 0,
+            TriPhasicMeasure<T> => 1,
+            _ => 2
+          })
         .Select(
           measure => measure switch
           {
@@ -259,12 +259,12 @@ public abstract record class PhasicMeasure<T>
     return this switch
     {
       CompositePhasicMeasure<T> composite => composite.Measures.OrderBy(
-            measure => measure switch
-            {
-              TriPhasicMeasure<T> => 0,
-              SinglePhasicMeasureSum<T> => 1,
-              _ => 2
-            })
+          measure => measure switch
+          {
+            TriPhasicMeasure<T> => 0,
+            SinglePhasicMeasureSum<T> => 1,
+            _ => 2
+          })
         .Select(
           measure => measure switch
           {
