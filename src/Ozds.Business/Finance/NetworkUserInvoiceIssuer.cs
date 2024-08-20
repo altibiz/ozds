a0d3767ed943a6cb593a2b3b8f6e8cc62ac4de7b
GabrijelAB <GabrijelAB@users.noreply.github.com>
2024-08-20 17:45:24 +0000

    format & deps.nix

--- src/Ozds.Business/Finance/NetworkUserInvoiceIssuer.cs
+++ src/Ozds.Business/Finance/NetworkUserInvoiceIssuer.cs
@@ -52,87 +52,87 @@ public class NetworkUserInvoiceIssuer(
         basis.ToDate,
         [
           new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture, "Network usage during one tariff period"),
-              localizer.ForCulture(culture, "Month"),
-              "1.1.",
-              1,
-              invoice.Invoice.UsageActiveEnergyTotalImportT0Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture, "Network usage during multiple tariff periods"),
-              localizer.ForCulture(culture, "Month"),
-              "1.2.",
-              1,
-              invoice.Invoice.UsageActiveEnergyTotalImportT1Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture, "Network usage during off-peak tariff period"),
-              localizer.ForCulture(culture, "Month"),
-              "1.3.",
-              1,
-              invoice.Invoice.UsageActiveEnergyTotalImportT2Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture, "Engaged power during multiple tariff periods"),
-              localizer.ForCulture(culture, "Month"),
-              "1.4.",
-              1,
-              invoice.Invoice.UsageActivePowerTotalImportT1PeakFee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(culture, "Excessively taken reactive power"),
-              localizer.ForCulture(culture, "Month"),
-              "1.5.",
-              1,
-              invoice.Invoice.UsageReactiveEnergyTotalRampedT0Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(culture, "Metering service fee"),
-              localizer.ForCulture(culture, "Month"),
-              "1.6.",
-              1,
-              invoice.Invoice.UsageMeterFee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture,
-                "Electricity supply during multi-rate daily tariff period"),
-              localizer.ForCulture(culture, "Month"),
-              "2.1.",
-              1,
-              invoice.Invoice.SupplyActiveEnergyTotalImportT1Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture,
-                "Electricity supply during off-peak daily tariff period"),
-              localizer.ForCulture(culture, "Month"),
-              "2.2.",
-              1,
-              invoice.Invoice.SupplyActiveEnergyTotalImportT2Fee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture,
-                "Fee for encouraging production from renewable energy sources and cogeneration"),
-              localizer.ForCulture(culture, "Month"),
-              "2.3.",
-              1,
-              invoice.Invoice.SupplyRenewableEnergyFee_EUR
-            ),
-            new AcknowledgeNetworkUserInvoiceItem(
-              localizer.ForCulture(
-                culture,
-                "Electricity supply during off-peak daily tariff period"),
-              localizer.ForCulture(culture, "Month"),
-              "2.4.",
-              1,
-              invoice.Invoice.SupplyBusinessUsageFee_EUR
-            )
+            localizer.ForCulture(
+              culture, "Network usage during one tariff period"),
+            localizer.ForCulture(culture, "Month"),
+            "1.1.",
+            1,
+            invoice.Invoice.UsageActiveEnergyTotalImportT0Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture, "Network usage during multiple tariff periods"),
+            localizer.ForCulture(culture, "Month"),
+            "1.2.",
+            1,
+            invoice.Invoice.UsageActiveEnergyTotalImportT1Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture, "Network usage during off-peak tariff period"),
+            localizer.ForCulture(culture, "Month"),
+            "1.3.",
+            1,
+            invoice.Invoice.UsageActiveEnergyTotalImportT2Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture, "Engaged power during multiple tariff periods"),
+            localizer.ForCulture(culture, "Month"),
+            "1.4.",
+            1,
+            invoice.Invoice.UsageActivePowerTotalImportT1PeakFee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(culture, "Excessively taken reactive power"),
+            localizer.ForCulture(culture, "Month"),
+            "1.5.",
+            1,
+            invoice.Invoice.UsageReactiveEnergyTotalRampedT0Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(culture, "Metering service fee"),
+            localizer.ForCulture(culture, "Month"),
+            "1.6.",
+            1,
+            invoice.Invoice.UsageMeterFee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture,
+              "Electricity supply during multi-rate daily tariff period"),
+            localizer.ForCulture(culture, "Month"),
+            "2.1.",
+            1,
+            invoice.Invoice.SupplyActiveEnergyTotalImportT1Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture,
+              "Electricity supply during off-peak daily tariff period"),
+            localizer.ForCulture(culture, "Month"),
+            "2.2.",
+            1,
+            invoice.Invoice.SupplyActiveEnergyTotalImportT2Fee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture,
+              "Fee for encouraging production from renewable energy sources and cogeneration"),
+            localizer.ForCulture(culture, "Month"),
+            "2.3.",
+            1,
+            invoice.Invoice.SupplyRenewableEnergyFee_EUR
+          ),
+          new AcknowledgeNetworkUserInvoiceItem(
+            localizer.ForCulture(
+              culture,
+              "Electricity supply during off-peak daily tariff period"),
+            localizer.ForCulture(culture, "Month"),
+            "2.4.",
+            1,
+            invoice.Invoice.SupplyBusinessUsageFee_EUR
+          )
         ],
         invoice.Invoice.Total_EUR,
         basis.RegulatoryCatalogue.TaxRate_Percent,
