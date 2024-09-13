c518b6d816374e94d8b58c74d390a3175a6f087d
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-13 16:51:28 +0200

    WIP

--- src/Ozds.Data/Extensions/MigrationBuilderExtensions.cs
+++ src/Ozds.Data/Extensions/MigrationBuilderExtensions.cs
@@ -1,323 +1,277 @@
 using System.Text;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Ozds.Data.Extensions;
-
-public static class MigrationBuilderExtensions
+namespace Ozds.Data.Extensions
 {
-  public static void ConvertIntToEnum(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName,
-    Dictionary<int, string> intToEnumMapping,
-    string enumTypeName
-  )
+  public static class MigrationBuilderExtensions
   {
-    var sql = new StringBuilder();
+    public static void ConvertIntToEnum(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<int, string> intToEnumMapping,
+        string enumTypeName
+    )
+    {
+      var sql = new StringBuilder();
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                ADD COLUMN temp_{
-                  columnName
-                } {
-                  enumTypeName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} {enumTypeName};
             ");
 
-    sql.AppendLine(
-      $@"
-                UPDATE {
-                  tableName
-                }
-                SET temp_{
-                  columnName
-                } = CASE");
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
 
-    foreach (var kvp in intToEnumMapping)
-    {
-      var enumIndex = kvp.Key;
-      var enumName = kvp.Value;
-      sql.AppendLine(
-        $@"
-                    WHEN {
-                      columnName
-                    } = {
-                      enumIndex
-                    } THEN '{
-                      enumName
-                    }'::{
-                      enumTypeName
-                    }");
-    }
+      foreach (var kvp in intToEnumMapping)
+      {
+        var enumIndex = kvp.Key;
+        var enumName = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN {columnName} = {enumIndex} THEN '{enumName}'::{enumTypeName}");
+      }
 
-    sql.AppendLine(
-      @"
+      sql.AppendLine($@"
                 END;
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                DROP COLUMN {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                RENAME COLUMN temp_{
-                  columnName
-                } TO {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
             ");
 
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertEnumToInt(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName,
-    Dictionary<string, int> enumToIntMapping
-  )
-  {
-    var sql = new StringBuilder();
-
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                ADD COLUMN temp_{
-                  columnName
-                } integer;
-            ");
-
-    sql.AppendLine(
-      $@"
-                UPDATE {
-                  tableName
-                }
-                SET temp_{
-                  columnName
-                } = CASE");
-
-    foreach (var kvp in enumToIntMapping)
-    {
-      var enumName = kvp.Key;
-      var enumIndex = kvp.Value;
-      sql.AppendLine(
-        $@"
-                    WHEN {
-                      columnName
-                    } = '{
-                      enumName
-                    }' THEN {
-                      enumIndex
-                    }");
+      migrationBuilder.Sql(sql.ToString());
     }
 
-    sql.AppendLine(
-      @"
+    public static void ConvertNullableIntToEnum(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<int, string> intToEnumMapping,
+        string enumTypeName
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} {enumTypeName};
+            ");
+
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
+
+      foreach (var kvp in intToEnumMapping)
+      {
+        var enumIndex = kvp.Key;
+        var enumName = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN {columnName} = {enumIndex} THEN '{enumName}'::{enumTypeName}");
+      }
+
+      sql.AppendLine($@"
+                    WHEN {columnName} IS NULL THEN NULL
                 END;
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                DROP COLUMN {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                RENAME COLUMN temp_{
-                  columnName
-                } TO {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
             ");
 
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertIntArrayToEnumArray(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName,
-    Dictionary<int, string> intToEnumMapping,
-    string enumTypeName
-  )
-  {
-    var sql = new StringBuilder();
-
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                ADD COLUMN temp_{
-                  columnName
-                } {
-                  enumTypeName
-                }[];
-            ");
-
-    sql.AppendLine(
-      $@"
-                UPDATE {
-                  tableName
-                }
-                SET temp_{
-                  columnName
-                } = ARRAY(
-                    SELECT CASE");
-
-    foreach (var kvp in intToEnumMapping)
-    {
-      var enumIndex = kvp.Key;
-      var enumName = kvp.Value;
-      sql.AppendLine(
-        $@"
-                    WHEN {
-                      columnName
-                    }_values = {
-                      enumIndex
-                    } THEN '{
-                      enumName
-                    }'::{
-                      enumTypeName
-                    }");
+      migrationBuilder.Sql(sql.ToString());
     }
 
-    sql.AppendLine(
-      $@"
-                    END
-                FROM unnest({
-                  columnName
-                }) AS {
-                  columnName
-                }_values
-              );
-            ");
-
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                DROP COLUMN {
-                  columnName
-                };
-            ");
-
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                RENAME COLUMN temp_{
-                  columnName
-                } TO {
-                  columnName
-                };
-            ");
-
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertEnumArrayToIntArray(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName,
-    Dictionary<string, int> enumToIntMapping
-  )
-  {
-    var sql = new StringBuilder();
-
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                ADD COLUMN temp_{
-                  columnName
-                } integer[];
-            ");
-
-    sql.AppendLine(
-      $@"
-                UPDATE {
-                  tableName
-                }
-                SET temp_{
-                  columnName
-                } = ARRAY(
-                    SELECT CASE");
-
-    foreach (var kvp in enumToIntMapping)
+    public static void ConvertEnumToInt(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<string, int> enumToIntMapping
+    )
     {
-      var enumName = kvp.Key;
-      var enumIndex = kvp.Value;
-      sql.AppendLine(
-        $@"
-                    WHEN {
-                      columnName
-                    }_values = '{
-                      enumName
-                    }' THEN {
-                      enumIndex
-                    }");
+      var sql = new StringBuilder();
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} integer;
+            ");
+
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
+
+      foreach (var kvp in enumToIntMapping)
+      {
+        var enumName = kvp.Key;
+        var enumIndex = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN {columnName} = '{enumName}' THEN {enumIndex}");
+      }
+
+      sql.AppendLine($@"
+                END;
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
     }
 
-    sql.AppendLine(
-      $@"
+    public static void ConvertEnumToNullableInt(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<string, int> enumToIntMapping
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} integer;
+            ");
+
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
+
+      foreach (var kvp in enumToIntMapping)
+      {
+        var enumName = kvp.Key;
+        var enumIndex = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN {columnName} = '{enumName}' THEN {enumIndex}");
+      }
+
+      sql.AppendLine($@"
+                    WHEN {columnName} IS NULL THEN NULL
+                END;
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
+    }
+
+    public static void ConvertIntArrayToEnumArray(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<int, string> intToEnumMapping,
+        string enumTypeName
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} {enumTypeName}[];
+            ");
+
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = ARRAY(
+                    SELECT CASE");
+
+      foreach (var kvp in intToEnumMapping)
+      {
+        var enumIndex = kvp.Key;
+        var enumName = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN x = {enumIndex} THEN '{enumName}'::{enumTypeName}");
+      }
+
+      sql.AppendLine($@"
                     END
-                FROM unnest({
-                  columnName
-                }) AS {
-                  columnName
-                }_values
-              );
+                    FROM unnest({columnName}) AS x
+                );
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                DROP COLUMN {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
             ");
 
-    sql.AppendLine(
-      $@"
-                ALTER TABLE {
-                  tableName
-                }
-                RENAME COLUMN temp_{
-                  columnName
-                } TO {
-                  columnName
-                };
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
             ");
 
-    migrationBuilder.Sql(sql.ToString());
+      migrationBuilder.Sql(sql.ToString());
+    }
+
+    public static void ConvertEnumArrayToIntArray(
+        this MigrationBuilder migrationBuilder,
+        string tableName,
+        string columnName,
+        Dictionary<string, int> enumToIntMapping
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} integer[];
+            ");
+
+      sql.AppendLine($@"
+                UPDATE {tableName}
+                SET temp_{columnName} = ARRAY(
+                    SELECT CASE");
+
+      foreach (var kvp in enumToIntMapping)
+      {
+        var enumName = kvp.Key;
+        var enumIndex = kvp.Value;
+        sql.AppendLine($@"
+                    WHEN x = '{enumName}' THEN {enumIndex}");
+      }
+
+      sql.AppendLine($@"
+                    END
+                    FROM unnest({columnName}) AS x
+                );
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine($@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
+    }
   }
 }
