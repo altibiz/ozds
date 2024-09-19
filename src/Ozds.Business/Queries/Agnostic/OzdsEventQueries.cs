210cec09c083b3d7853ad60d11686bf471b57238
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-19 19:25:00 +0200

    WIP

--- src/Ozds.Business/Queries/Agnostic/OzdsEventQueries.cs
+++ src/Ozds.Business/Queries/Agnostic/OzdsEventQueries.cs
@@ -10,22 +10,21 @@ using IEvent = Ozds.Business.Models.Abstractions.IEvent;
 namespace Ozds.Business.Queries.Agnostic;
 
 public class OzdsEventQueries(
-  DataDbContext context,
+  IDbContextFactory<DataDbContext> contextFactory,
   AgnosticModelEntityConverter modelEntityConverter
 ) : IQueries
 {
-  private readonly DataDbContext _context = context;
-
   private readonly AgnosticModelEntityConverter _modelEntityConverter =
     modelEntityConverter;
 
   public async Task<T?> ReadSingle<T>(string id)
     where T : class, IEvent
   {
+    await using var context = await contextFactory.CreateDbContextAsync();
     var entityType = _modelEntityConverter.EntityType(typeof(T));
-    var queryable = _context.GetQueryable(entityType);
+    var queryable = context.GetQueryable(entityType);
     var item = await queryable
-      .Where(_context.PrimaryKeyEquals(entityType, id))
+      .Where(context.PrimaryKeyEquals(entityType, id))
       .FirstOrDefaultAsync();
     return item is null ? null : _modelEntityConverter.ToModel(item) as T;
   }
@@ -37,8 +36,9 @@ public class OzdsEventQueries(
   )
     where T : class, IEvent
   {
+    await using var context = await contextFactory.CreateDbContextAsync();
     var dbSetType = _modelEntityConverter.EntityType(typeof(T));
-    var queryable = _context.GetQueryable(dbSetType)
+    var queryable = context.GetQueryable(dbSetType)
         as IQueryable<EventEntity>
       ?? throw new InvalidOperationException(
         $"No DbSet found for {dbSetType}");
