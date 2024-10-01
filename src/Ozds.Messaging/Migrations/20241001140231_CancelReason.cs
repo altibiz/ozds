11124ea81318944ed50f67fc16342dd2e5acf29c
Hrvoje Jurić <hrvoje@altibiz.com>
2024-10-01 16:05:11 +0200

    WIP

---
+++ src/Ozds.Messaging/Migrations/20241001140231_CancelReason.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ozds.Messaging.Migrations
+{
+    /// <inheritdoc />
+    public partial class CancelReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cancel_reason",
+                table: "network_user_invoice_state",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cancel_reason",
+                table: "network_user_invoice_state");
+        }
+    }
+}
