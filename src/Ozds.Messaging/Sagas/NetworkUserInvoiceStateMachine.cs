18653ba65d4ef72b07a154bf1a987e608f6f58c2
HrvojeJuric <HrvojeJuric@users.noreply.github.com>
2024-10-04 10:51:35 +0000

    format & deps.nix

--- src/Ozds.Messaging/Sagas/NetworkUserInvoiceStateMachine.cs
+++ src/Ozds.Messaging/Sagas/NetworkUserInvoiceStateMachine.cs
@@ -7,82 +7,80 @@ namespace Ozds.Messaging.Sagas;
 public class NetworkUserInvoiceStateMachine
   : MassTransitStateMachine<NetworkUserInvoiceStateEntity>
 {
-    public NetworkUserInvoiceStateMachine()
-    {
-        InstanceState(x => x.CurrentState);
+  public NetworkUserInvoiceStateMachine()
+  {
+    InstanceState(x => x.CurrentState);
 
-        Event(
-          () => InitiateNetworkUserInvoice,
-          x =>
-          {
-              x.CorrelateBy(
-            (state, context) =>
-              state.NetworkUserInvoiceId
-              == context.Message.NetworkUserInvoiceId);
-              x.SelectId(x => NewId.NextGuid());
+    Event(
+      () => InitiateNetworkUserInvoice,
+      x =>
+      {
+        x.CorrelateBy(
+          (state, context) =>
+            state.NetworkUserInvoiceId
+            == context.Message.NetworkUserInvoiceId);
+        x.SelectId(x => NewId.NextGuid());
 
-              x.InsertOnInitial = true;
+        x.InsertOnInitial = true;
 
-              x.SetSagaFactory(
-            context =>
-              new NetworkUserInvoiceStateEntity
-                {
-                    CorrelationId = context.CorrelationId ?? NewId.NextGuid(),
-                    NetworkUserInvoiceId = context.Message.NetworkUserInvoiceId
-                });
-          });
+        x.SetSagaFactory(
+          context =>
+            new NetworkUserInvoiceStateEntity
+            {
+              CorrelationId = context.CorrelationId ?? NewId.NextGuid(),
+              NetworkUserInvoiceId = context.Message.NetworkUserInvoiceId
+            });
+      });
 
-        Event(
-          () => AbortNetworkUserInvoice,
-          x => x
-            .CorrelateBy(
-              (state, context) =>
-                state.NetworkUserInvoiceId
-                == context.Message.NetworkUserInvoiceId)
-            .SelectId(
-              x => NewId.NextGuid()));
+    Event(
+      () => AbortNetworkUserInvoice,
+      x => x
+        .CorrelateBy(
+          (state, context) =>
+            state.NetworkUserInvoiceId
+            == context.Message.NetworkUserInvoiceId)
+        .SelectId(
+          x => NewId.NextGuid()));
 
-        Event(
-          () => RegisterNetworkUserInvoice,
-          x => x
-            .CorrelateBy(
-              (state, context) =>
-                state.NetworkUserInvoiceId
-                == context.Message.NetworkUserInvoiceId)
-            .SelectId(
-              x => NewId.NextGuid()));
+    Event(
+      () => RegisterNetworkUserInvoice,
+      x => x
+        .CorrelateBy(
+          (state, context) =>
+            state.NetworkUserInvoiceId
+            == context.Message.NetworkUserInvoiceId)
+        .SelectId(
+          x => NewId.NextGuid()));
 
-        Initially(
-          When(InitiateNetworkUserInvoice)
-            .TransitionTo(Initiated));
+    Initially(
+      When(InitiateNetworkUserInvoice)
+        .TransitionTo(Initiated));
 
-        DuringAny(
-          When(AbortNetworkUserInvoice)
-            .Then(context => context.Saga.AbortReason = context.Message.Reason)
-            .TransitionTo(Aborted));
+    DuringAny(
+      When(AbortNetworkUserInvoice)
+        .Then(context => context.Saga.AbortReason = context.Message.Reason)
+        .TransitionTo(Aborted));
 
-        During(
-          Initiated,
-          When(RegisterNetworkUserInvoice)
-            .Then(context => context.Saga.BillId = context.Message.BillId)
-            .Activity(x => x.OfType<NetworkUserInvoiceRegisteredActivity>())
-            .TransitionTo(Registered));
-    }
+    During(
+      Initiated,
+      When(RegisterNetworkUserInvoice)
+        .Then(context => context.Saga.BillId = context.Message.BillId)
+        .Activity(x => x.OfType<NetworkUserInvoiceRegisteredActivity>())
+        .TransitionTo(Registered));
+  }
 
-    public State Initiated { get; } = default!;
+  public State Initiated { get; } = default!;
 
-    public State Aborted { get; } = default!;
+  public State Aborted { get; } = default!;
 
-    public State Registered { get; } = default!;
+  public State Registered { get; } = default!;
 
-    public Event<IInitiateNetworkUserInvoice>
-      InitiateNetworkUserInvoice
-    { get; } = default!;
+  public Event<IInitiateNetworkUserInvoice>
+    InitiateNetworkUserInvoice { get; } = default!;
 
-    public Event<IAbortNetworkUserInvoice> AbortNetworkUserInvoice { get; } =
-      default!;
+  public Event<IAbortNetworkUserInvoice> AbortNetworkUserInvoice { get; } =
+    default!;
 
-    public Event<IRegisterNetworkUserInvoice>
-      RegisterNetworkUserInvoice
-    { get; } = default!;
+  public Event<IRegisterNetworkUserInvoice>
+    RegisterNetworkUserInvoice { get; } = default!;
 }
