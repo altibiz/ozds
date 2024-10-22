28979cffe5ddf52ab47bcfd520440955330a466c
Hrvoje Juric <hrvoje@altibiz.com>
2024-10-22 21:41:30 +0200

    WIP

--- test/Ozds.Data.Test/Queries/AnalysisQueriesTest/ReadAnalysisBasesByRepresentativeTest.cs
+++ test/Ozds.Data.Test/Queries/AnalysisQueriesTest/ReadAnalysisBasesByRepresentativeTest.cs
@@ -1,1 +1,27 @@
+using Ozds.Data.Entities.Enums;
+using Ozds.Data.Queries;
+
 namespace Ozds.Data.Test.AnalysisQueriesTest;
+
+public class ReadAnalysisBasesByRepresentativeTest
+{
+  [Fact]
+  public async Task IsValidForOperator()
+  {
+    var serviceCollection = new ServiceCollection();
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+
+    var queries = ActivatorUtilities
+      .CreateInstance<AnalysisQueries>(serviceProvider);
+
+    var result = await queries.ReadAnalysisBasesByRepresentative(
+      "id",
+      RoleEntity.OperatorRepresentative,
+      DateTimeOffset.MinValue,
+      DateTimeOffset.MaxValue,
+      CancellationToken.None
+    );
+
+    Assert.NotNull(result);
+  }
+}
