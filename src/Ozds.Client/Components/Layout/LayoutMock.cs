68752de8d069c32b01ada022b9fda64a8c789558
Gabrijel Čmelić <gabrijel@altibiz.com>
2024-11-14 11:33:27 +0100

    WIP

--- src/Ozds.Client/Components/Layout/LayoutMock.cs
+++ src/Ozds.Client/Components/Layout/LayoutMock.cs
@@ -14,6 +14,14 @@ namespace Ozds.Client.Components.LayoutMock;
 
 public partial class LayoutMock : OzdsLayoutComponentBase
 {
+  [CascadingParameter]
+  private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+  [Inject]
+  private IServiceScopeFactory ServiceScopeFactory { get; set; } = default!;
+
+  private LayoutState LayoutState { get; set; } = default!;
+
   private ThemeState ThemeState { get; set; } = default!;
 
 #pragma warning disable S4487 // Unread "private" fields should be removed
@@ -21,6 +29,36 @@ public partial class LayoutMock : OzdsLayoutComponentBase
 
   protected override void OnInitialized()
   {
+    LayoutState = new LayoutState(
+      IsUserDrawerOpen: false,
+      IsLocalizationDrawerOpen: false,
+      IsNavigationDrawerOpen: false,
+      SetUserDrawerOpen: isUserDrawerOpen =>
+      {
+        LayoutState = LayoutState with
+        {
+          IsUserDrawerOpen = isUserDrawerOpen
+        };
+        InvokeAsync(StateHasChanged);
+      },
+      SetLocalizationDrawerOpen: isLocalizationDrawerOpen =>
+      {
+        LayoutState = LayoutState with
+        {
+          IsLocalizationDrawerOpen = isLocalizationDrawerOpen
+        };
+        InvokeAsync(StateHasChanged);
+      },
+      SetNavigationDrawerOpen: isNavigationDrawerOpen =>
+      {
+        LayoutState = LayoutState with
+        {
+          IsNavigationDrawerOpen = isNavigationDrawerOpen
+        };
+        InvokeAsync(StateHasChanged);
+      }
+    );
+
     ThemeState = new ThemeState(
       Theme: ThemeState.Default(),
       IsDarkMode: false,
@@ -35,5 +73,53 @@ public partial class LayoutMock : OzdsLayoutComponentBase
         InvokeAsync(StateHasChanged);
       }
     );
+  }
+
+  private async Task<(UserState? UserState, RepresentativeState? RepresentativeState)> LoadAsync()
+  {
+    if (AuthenticationStateTask is null)
+    {
+      return default;
+    }
+
+    var authenticationState = await AuthenticationStateTask;
+    var claimsPrincipal = authenticationState?.User ??
+      throw new InvalidOperationException(
+        "No claims principal found.");
+    if (!(claimsPrincipal.Identity?.IsAuthenticated ?? false))
+    {
+      NavigateToLogin();
+      return default;
+    }
+
+    await using var scope = ServiceScopeFactory.CreateAsyncScope();
+    var maybeRepresentingUser = await scope.ServiceProvider
+      .GetRequiredService<OzdsRepresentativeQueries>()
+      .MaybeRepresentingUserByClaimsPrincipal(claimsPrincipal);
+    if (maybeRepresentingUser is null)
+    {
+      NavigateToLogin();
+      return default;
+    }
+
+    var userState = new UserState(claimsPrincipal, maybeRepresentingUser.User);
+    if (maybeRepresentingUser.Representative is not { } representative)
+    {
+      return (userState, null);
+    }
+
+    var representativeState =
+      new RepresentativeState(
+        representative,
+        new(),
+        new(),
+        (notification) =>
+        {
+        },
+        (notification) =>
+        {
+        }
+      );
+    return (userState, representativeState);
   }
 }
