741a91277dc0dfd796537e757167e9e9de9ebac8
Hrvoje Jurić <hrvoje@altibiz.com>
2024-11-15 12:29:15 +0100

    WIP

--- test/Ozds.Data.Test/Fixtures/AnalysisBasisEntityFactory.cs
+++ test/Ozds.Data.Test/Fixtures/AnalysisBasisEntityFactory.cs
@@ -181,7 +181,8 @@ public class AnalysisBasisEntityFactory(DbContext context)
               .MaxBy(y => y
                 .ToModel()
                 .ActivePower_W
-                .TariffUnary()
+                .TariffBinary()
+                .T1
                 .DuplexImport()
                 .PhaseSum()))
             .Cast<AggregateEntity>()
