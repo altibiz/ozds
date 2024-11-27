8ac8f015e32e72afc6aee4973492e321e5fae15c
Hrvoje Jurić <hrvoje@altibiz.com>
2024-11-27 10:15:24 +0100

    WIP

--- src/Ozds.Client/Components/Streaming/Table.cs
+++ src/Ozds.Client/Components/Streaming/Table.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Ozds.Business.Models.Abstractions;
+using Ozds.Client.Components.Base;
 
 namespace Ozds.Client.Components.Streaming;
 
-public partial class Table<T>
+public partial class Table<T> : OzdsComponentBase
 {
   [Parameter]
   public List<T> Model { get; set; } = default!;
-
-  [Parameter]
-  public RenderFragment Columns { get; set; } = default!;
 
   [Parameter]
   public RenderFragment<T> Summary { get; set; } = default!;
@@ -18,9 +16,22 @@ public partial class Table<T>
   public RenderFragment<T> Details { get; set; } = default!;
 
   [Parameter]
+  public RenderFragment Columns { get; set; } = default!;
+
+  [Parameter]
   public string Link { get; set; } = default!;
 
+  [Parameter]
+  public RenderFragment<T>? Title { get; set; } = default!;
+
   private string? _searchString;
+
+  private List<TableItemMetadata> _metadata = new();
+
+  protected override void OnParametersSet()
+  {
+    _metadata = Model.Select(x => new TableItemMetadata(false)).ToList();
+  }
 
   private bool Filter(T value)
   {
@@ -39,4 +50,6 @@ public partial class Table<T>
 
     return false;
   }
+
+  private sealed record TableItemMetadata(bool Expanded);
 }
