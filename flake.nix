9a0a865fad46802773abd74d6a257ba905b83ea2
Hrvoje Jurić <hrvoje@altibiz.com>
2024-12-13 11:47:20 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@ rec {
   description = "OZDS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     flake-utils.url = "github:numtide/flake-utils";
 
@@ -201,6 +201,7 @@ rec {
               usql
               postgresql_14
               mermerd
+              sql-formatter
 
               # MailHog
               apacheHttpd
