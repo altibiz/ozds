8f3831eeca7dda30ed1d303731ce2a539041d7d5
Hrvoje Jurić <hrvoje@altibiz.com>
2025-01-06 14:37:59 +0100

    WIP

--- src/Ozds.Messaging/Queries/NetworkUserInvoiceStateQueries.cs
+++ src/Ozds.Messaging/Queries/NetworkUserInvoiceStateQueries.cs
@@ -6,15 +6,19 @@ using Ozds.Messaging.Queries.Abstractions;
 namespace Ozds.Messaging.Queries;
 
 public class NetworkUserInvoiceStateQueries(
-  MessagingDbContext context
+  IDbContextFactory<MessagingDbContext> factory
 ) : IQueries
 {
   public async Task<NetworkUserInvoiceStateEntity?> ReadAsync(
-    string networkUserInvoiceId
+    string networkUserInvoiceId,
+    CancellationToken cancellationToken
   )
   {
+    await using var context = await factory
+      .CreateDbContextAsync(cancellationToken);
+
     return await context.NetworkUserInvoiceStates
       .Where(x => x.NetworkUserInvoiceId == networkUserInvoiceId)
-      .FirstOrDefaultAsync();
+      .FirstOrDefaultAsync(cancellationToken);
   }
 }
