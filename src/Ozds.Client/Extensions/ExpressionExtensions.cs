97389dbc7c6268200f24d8a33be9fd84404ea01d
Hrvoje Jurić <hrvoje@altibiz.com>
2025-01-27 11:26:57 +0100

    WIP

---
+++ src/Ozds.Client/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+
+namespace Ozds.Client.Extensions;
+
+public static class ExpressionExtensions
+{
+  public static string Label<TIn, TOut>(
+      this Expression<Func<TIn, TOut>> expression
+  )
+  {
+    if (expression.Body is MemberExpression memberExpression)
+    {
+      return memberExpression.Member.Name;
+    }
+
+    if (expression.Body is UnaryExpression unaryExpression
+      && unaryExpression.NodeType == ExpressionType.Convert
+      && unaryExpression.Operand is MemberExpression innerMemberExpression)
+    {
+      return innerMemberExpression.Member.Name;
+    }
+
+    return "";
+  }
+}
