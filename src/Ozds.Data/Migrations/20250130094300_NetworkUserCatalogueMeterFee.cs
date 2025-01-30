593dbd379f5a94d0e009bec398198fa32bb35444
Hrvoje Jurić <hrvoje@altibiz.com>
2025-01-30 10:44:34 +0100

    WIP

---
+++ src/Ozds.Data/Migrations/20250130094300_NetworkUserCatalogueMeterFee.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ozds.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NetworkUserCatalogueMeterFee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "meter_fee_price_eur",
+                table: "network_user_catalogues",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "meter_fee_price_eur",
+                table: "network_user_catalogues",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
