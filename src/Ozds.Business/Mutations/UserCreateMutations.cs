446fab4c43e2b218bdc6877e18456cfc353c264c
GabrijelAB <GabrijelAB@users.noreply.github.com>
2025-02-07 09:44:55 +0000

    format & deps.nix

--- src/Ozds.Business/Mutations/UserCreateMutations.cs
+++ src/Ozds.Business/Mutations/UserCreateMutations.cs
@@ -2,18 +2,17 @@ using OrchardCore.Users.Models;
 using Ozds.Users.Mutations.Abstractions;
 using DataUserCreateMutations = Ozds.Users.Mutations.CreateMutations;
 
-namespace Ozds.Business.Mutations
+namespace Ozds.Business.Mutations;
+
+public class UserCreateMutations(DataUserCreateMutations mutations)
+  : IMutations
 {
-  public class UserCreateMutations(DataUserCreateMutations mutations)
-    : IMutations
+  public async Task<User> CreateUser(
+    string userName,
+    string userEmail,
+    CancellationToken cancellationToken
+  )
   {
-    public async Task<User> CreateUser(
-      string userName,
-      string userEmail,
-      CancellationToken cancellationToken
-    )
-    {
-      return await mutations.CreateUser(userName, userEmail, cancellationToken);
-    }
+    return await mutations.CreateUser(userName, userEmail, cancellationToken);
   }
 }
