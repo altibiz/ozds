72d3a1e6370d2f23bfe826bdfb7f12d45ec79891
Hrvoje Jurić <hrvoje@altibiz.com>
2025-02-10 11:07:21 +0100

    WIP

--- src/Ozds.Document/Entities/CalculatedNetworkUserInvoiceEntity.cs
+++ src/Ozds.Document/Entities/CalculatedNetworkUserInvoiceEntity.cs
@@ -1,6 +1,6 @@
 namespace Ozds.Document.Entities;
 
-public record CalculatedNetworkUserInvoiceEntity
+public class CalculatedNetworkUserInvoiceEntity
 {
   public List<NetworkUserCalculationEntity> Calculations { get; set; } = default!;
 
