38a07a8975cecd1d9fb7590837f70063ca229d15
Gabrijel Čmelić <Gabrijel1207@gmail.com>
2025-03-12 17:53:48 +0100

    Code fixes

---
+++ src/Ozds.Client/Pages/Measurements/MeasurementLocationPage.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Ozds.Business.Analysis;
+using Ozds.Business.Models.Abstractions;
+using Ozds.Business.Models.Enums;
+using Ozds.Business.Queries;
+using Ozds.Business.Time;
+using Ozds.Client.Components.Models.Base;
+using Ozds.Client.State;
+
+namespace Ozds.Client.Pages;
+
+public partial class MeasurementLocationPage
+  : OzdsIdentifiableModelPageComponentBase<IMeasurementLocation>
+{
+  [CascadingParameter]
+  public AnalysisState AnalysisState { get; set; } = default!;
+
+  [CascadingParameter]
+  public Breakpoint Breakpoint { get; set; }
+
+  [Parameter]
+  public string Id { get; set; } = default!;
+
+  private MeasurementLocationAnalysis? analysis;
+
+  private List<IMeasurementLocation> measurementLocations = new();
+
+  IEnumerable<IAggregate> measurements = new List<IAggregate>();
+
+  private DateTime? selectedMonth;
+
+  private async Task OnDateChanged(DateTime? date)
+  {
+    selectedMonth = date;
+    if (analysis is not null)
+    {
+      measurementLocations = new List<IMeasurementLocation> { analysis.MeasurementLocation };
+      DateTimeOffset dto = new DateTimeOffset(selectedMonth!.Value, TimeSpan.Zero);
+      var queries = ScopedServices.GetRequiredService<MeasurementQueries>();
+      var measures = await queries.ReadByMeasurementLocationIdsDynamic(
+        measurementLocations,
+        ResolutionModel.Hour,
+        30,
+        0,
+        CancellationToken,
+        dto.GetStartOfMonth(),
+        dto.GetStartOfNextMonth(),
+        5000);
+      var orderedMeasures = measures.Items.OrderBy(x => x.Timestamp).ToList();
+      measurements = orderedMeasures.Select(x => (IAggregate)x).ToList();
+    }
+  }
+
+  protected override async Task OnParametersSetAsync()
+  {
+    analysis = AnalysisState.AnalysisBases.Value
+      .AnalysesByMeasurementLocation()
+      .FirstOrDefault(x => x.MeasurementLocation.Id == Id);
+
+    if (measurements.Any())
+    {
+      await OnDateChanged(selectedMonth);
+    }
+  }
+}
---
+++ src/Ozds.Client/Pages/Measurements/MeterPage.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Ozds.Business.Analysis;
+using Ozds.Business.Models.Abstractions;
+using Ozds.Business.Models.Enums;
+using Ozds.Business.Queries;
+using Ozds.Business.Time;
+using Ozds.Client.Components.Models.Base;
+using Ozds.Client.State;
+
+namespace Ozds.Client.Pages;
+
+public partial class MeterPage
+  : OzdsIdentifiableModelPageComponentBase<IMeter>
+{
+
+  [CascadingParameter]
+  private AnalysisState AnalysisState { get; set; } = default!;
+
+  [Parameter]
+  public string Id { get; set; } = default!;
+
+  [CascadingParameter]
+  private RepresentativeState RepresentativeState { get; set; } = default!;
+
+  [CascadingParameter]
+  private Breakpoint Breakpoint { get; set; }
+
+  private MeterAnalysis? analysis;
+
+  private List<IMeter> meters = new();
+
+  IEnumerable<object> measurements = new List<object>();
+
+  private DateTime? selectedMonth;
+
+  private async Task OnDateChanged(DateTime? date)
+  {
+    selectedMonth = date;
+    if (analysis is not null)
+    {
+      meters = new List<IMeter> { analysis.Meter };
+      DateTimeOffset dto = new DateTimeOffset(selectedMonth!.Value, TimeSpan.Zero);
+      var queries = ScopedServices.GetRequiredService<MeasurementQueries>();
+      var measures = await queries.ReadByMeterIdsDynamic(
+        meters,
+        ResolutionModel.Hour,
+        30,
+        0,
+        CancellationToken,
+        dto.GetStartOfMonth(),
+        dto.GetStartOfNextMonth(),
+        5000);
+      measurements = measures.Items.OrderBy(x => x.Timestamp).ToList();
+    }
+  }
+
+  protected override async Task OnParametersSetAsync()
+  {
+    analysis = AnalysisState.AnalysisBases.Value
+      .AnalysesByMeter()
+      .FirstOrDefault(x => x.Meter.Id == Id);
+
+    if (measurements.Any())
+    {
+      await OnDateChanged(selectedMonth);
+    }
+  }
+}
