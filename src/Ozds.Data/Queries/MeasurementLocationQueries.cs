19f712fad1b2cf269bc029753a3d6be495f74c5e
Gabrijel Čmelić <Gabrijel1207@gmail.com>
2025-03-17 15:07:49 +0100

    WIP

--- src/Ozds.Data/Queries/MeasurementLocationQueries.cs
+++ src/Ozds.Data/Queries/MeasurementLocationQueries.cs
@@ -73,20 +73,11 @@ public class MeasurementLocationQueries(
     return measurementLocations;
   }
 
-<<<<<<< HEAD
   public async Task<List<IMeasurementLocationEntity>?>
     ReadMeasurementLocationByLocation(
       string locationId,
       CancellationToken cancellationToken
     )
-=======
-  public async Task<
-    List<IMeasurementLocationEntity>
-  > ReadMeasurementLocationByLocation(
-    string locationId,
-    CancellationToken cancellationToken
-  )
->>>>>>> 4161e995 (More pr comment fixes)
   {
     await using var context = await factory.CreateDbContextAsync(
       cancellationToken
