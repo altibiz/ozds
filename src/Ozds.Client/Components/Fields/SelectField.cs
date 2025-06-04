41ccc402be45fcbaeb4f046660e6a02d92213e6c
GabrijelAB <155453176+GabrijelAB@users.noreply.github.com>
2025-06-04 12:14:54 +0200

    196 bug mobile drop down (#202)
    ### Added
    
    - tool tip logic for measurement selection dropdown
    - text display logic for measurement selection dropdown
    
    ### Changed
    
    - all selection elements work on mobile with new SelectField component
    - turned on detailed errors for blazor while debugging

---
+++ src/Ozds.Client/Components/Fields/SelectField.cs
@@ -0,0 +1,41 @@
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Ozds.Client.Components.Fields;
+
+public partial class SelectField<T>
+{
+  private MudSelect<T> _inner = default!;
+
+  [Parameter]
+  public string Label { get; set; } = default!;
+
+  [Parameter]
+  public T Value { get; set; } = default!;
+
+  [Parameter]
+  public EventCallback<T> ValueChanged { get; set; }
+
+  [Parameter]
+  public IEnumerable<T> SelectedValues { get; set; } = Enumerable.Empty<T>();
+
+  [Parameter]
+  public EventCallback<IEnumerable<T>> SelectedValuesChanged { get; set; }
+
+  [Parameter]
+  public bool MultiSelection { get; set; } = false;
+
+  [Parameter]
+  public Expression<Func<T>> For { get; set; } = default!;
+
+  [Parameter]
+  public Origin AnchorOrigin { get; set; } = Origin.BottomLeft;
+
+  [Parameter(CaptureUnmatchedValues = true)]
+  public IDictionary<string, object> AdditionalAttributes { get; set; } =
+    new Dictionary<string, object>();
+
+  [Parameter]
+  public RenderFragment ChildContent { get; set; } = default!;
+}
