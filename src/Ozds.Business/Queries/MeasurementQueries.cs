d02e90d384dc95e55ef02ee347f4751427f5766e
Gabrijel Čmelić <Gabrijel1207@gmail.com>
2025-07-02 14:24:49 +0200

    WIP

--- src/Ozds.Business/Queries/MeasurementQueries.cs
+++ src/Ozds.Business/Queries/MeasurementQueries.cs
@@ -71,14 +71,13 @@ public class MeasurementQueries(
           )
           : modelEntityConverter.EntityType(
             meterNamingConvention.MeasurementTypeForMeterId(id)
-          ));
-    var a = modelIdsByEntityType
+          ))
       .Select(
         group =>
           new KeyValuePair<Type, IEnumerable<string>>(group.Key, group)).ToList();
 
     var entities = await queries.ReadByMeterIds(
-      a,
+      modelIdsByEntityType,
       appropriateInterval,
       fromDate,
       toDate,
