02e8c48a8de4c401ffa44b9a17d860fb8b4f9390
Hrvoje Juric <hrvoje@altibiz.com>
2025-07-08 20:26:20 +0200

    cleanup analysis

--- src/Ozds.Business/Analysis/Analysis.cs
+++ src/Ozds.Business/Analysis/Analysis.cs
@@ -43,3 +43,42 @@ public record MeterAnalysis(
   MeterModel Meter,
   Analysis Analysis
 );
+
+public record Consumption(
+  DateTimeOffset Timestamp,
+  decimal MinActiveEnergy_kWh,
+  decimal MaxActiveEnergy_kWh,
+  decimal ActiveEnergy_kWh,
+  decimal MinReactiveEnergy_kVARh,
+  decimal MaxReactiveEnergy_kVARh,
+  decimal ReactiveEnergy_kVARh,
+  decimal MinApparentEnergy_kVAh,
+  decimal MaxApparentEnergy_kVAh,
+  decimal ApparentEnergy_kVAh
+)
+{
+  public static readonly Consumption Null = new(
+    DateTimeOffset.MinValue,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  );
+}
+
+public record Expenses(
+  DateTimeOffset Timestamp,
+  decimal Total_EUR
+);
+
+public record Load(
+  DateTimeOffset Timestamp,
+  decimal ActivePower_kW,
+  decimal ReactivePower_kVAR,
+  decimal ApparentPower_kVA
+);
