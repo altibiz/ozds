b615371b34c81031fc9345dfc24a3811d6ffd9bd
Hrvoje Juric <hrvoje@altibiz.com>
2026-01-21 08:56:53 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgsPlaywright.url = "github:NixOS/nixpkgs?rev=d02d88f8de5b882ccdde0465d8fa2db3aa1169f7";
 
     poetry2nix.url = "github:nix-community/poetry2nix";
