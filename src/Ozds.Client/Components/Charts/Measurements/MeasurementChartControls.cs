f07b34c1a3947e10696b0b1450c1edc8f4c516a0
Ivan Benčić <80389344+Ivek-rgb@users.noreply.github.com>
2026-02-02 09:52:41 +0100

    fixed unintended reset of the selected measurement points or meters on component parameters change (#246)
    ### Changed
    
    - fixed unintended reset of the selected measurement points or meters on each
      property change (for now it's local fix but it could be done so that the state
      and params of chart control persist after reloading the page)

--- src/Ozds.Client/Components/Charts/Measurements/MeasurementChartControls.cs
+++ src/Ozds.Client/Components/Charts/Measurements/MeasurementChartControls.cs
@@ -14,6 +14,7 @@ namespace Ozds.Client.Components.Charts;
 
 public partial class MeasurementChartControls : OzdsComponentBase
 {
+  private bool _initParamsSet = false;
   private MeasurementChartParameters _parameters = new();
 
   private MudSelect<string> _select = default!;
@@ -85,20 +86,44 @@ public partial class MeasurementChartControls : OzdsComponentBase
 
   protected override void OnParametersSet()
   {
+    if (_parameters.MeasurementLocations.Count == 0
+      && _parameters.Meters.Count == 0
+      && _initParamsSet)
+    {
+      return;
+    }
+
     if (MeasurementLocations.Count == 1)
     {
       _parameters.MeasurementLocations = MeasurementLocations.ToHashSet();
+      _initParamsSet = true;
       return;
     }
 
     if (Meters.Count == 1)
     {
       _parameters.Meters = Meters.ToHashSet();
+      _initParamsSet = true;
+      return;
+    }
+
+    if (_parameters.MeasurementLocations.Count > 0)
+    {
+      _parameters.MeasurementLocations
+        .IntersectWith(MeasurementLocations);
+      return;
+    }
+
+    if (_parameters.Meters.Count > 0)
+    {
+      _parameters.Meters
+        .IntersectWith(Meters);
       return;
     }
 
     if (MeasurementLocations.Count > 0)
     {
+      _initParamsSet = true;
       _parameters.MeasurementLocations = MeasurementLocations
         .Take(1)
         .ToHashSet();
@@ -107,6 +132,8 @@ public partial class MeasurementChartControls : OzdsComponentBase
 
     if (Meters.Count > 0)
     {
+      _initParamsSet = true;
+
       _parameters.Meters = Meters
         .Take(1)
         .ToHashSet();
